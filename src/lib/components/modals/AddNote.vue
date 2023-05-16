9cfba406bd56ee0d4f7bf3075847ad78969c6267
Pierre Wasilewski <pierrewasilewski@MacBook-Air-de-Pierre.local>
2023-05-16 11:08:28 +0200

    :sparkles: Adding emitter,modal and form for note creation.

---
+++ src/lib/components/modals/AddNote.vue
@@ -0,0 +1,25 @@
+<template>
+  <VueFinalModal
+    class="confirm-modal"
+    content-class="confirm-modal-content"
+    overlay-transition="vfm-fade"
+    content-transition="vfm-fade"
+  >
+    <satisfaction-form @success="success" />
+  </VueFinalModal>
+</template>
+
+<script setup lang="ts">
+import { VueFinalModal } from "vue-final-modal";
+import AddNote from "@/components/form/AddNote.vue";
+
+const emit = defineEmits<{
+  (e: "close"): void;
+}>();
+const success = () => {
+  emit("close");
+  useToasteoSuccess();
+};
+</script>
+
+<style></style>
---
+++ src/lib/utils/emitter.ts
@@ -0,0 +1,26 @@
+import { useModal } from "vue-final-modal";
+import type { Constructor } from "vue-final-modal";
+import AddNote from "@/components/modals/AddNote.vue";
+
+const useComponentModal = <P extends Record<string, unknown>>(
+  modalComponent: Constructor,
+  props: P
+) => {
+  const instance = useModal({
+    component: modalComponent,
+    attrs: {
+      ...props,
+      onClose() {
+        instance.close();
+      },
+      // onBeforeClose() {
+      //   loadStop();
+      // },
+    },
+  });
+  return instance;
+};
+
+export const useAddNote = () => {
+  return useComponentModal(AddNote, {});
+};
