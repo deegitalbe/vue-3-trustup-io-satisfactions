298d165fe1dc5deb7bb40514eb3ae58aa8f4f077
Pierre Wasilewski <pierrewasilewski@MacBook-Air-de-Pierre.local>
2023-06-21 16:17:49 +0200

    :construction: Adding translation key

--- src/lib/plugins/satisfactionPlugin.ts
+++ src/lib/plugins/satisfactionPlugin.ts
@@ -1,10 +1,10 @@
 // import { createApp } from "vue";
 import { Plugin } from "vue";
-// import { useTranslation } from "@deegital/vue-3-trustup-io-translations";
+import { useTranslation } from "@deegital/vue-3-trustup-io-translations";
 
 const satisfactionPlugin: Plugin = {
   install: () => {
-    // useTranslation().addTranslationsByKey("worksite-admin");
+    useTranslation().addTranslationsByKey("worksite-admin");
   },
 };
 
